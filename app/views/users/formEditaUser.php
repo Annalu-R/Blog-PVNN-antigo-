1a72089fe0729ed04009f7d2c38f1046629e3a46
Annalu-R <55161495+Annalu-R@users.noreply.github.com>
2022-09-02 17:41:05 -0300

    Update and rename app/views/clientes/formEditaCliente.php to app/views/users/formEditaUser.php

---
+++ app/views/users/formEditaUser.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+	include_once __DIR__ . "/../helpers/mensagem.php";
+	//$caminho = __DIR__ . "/../helpers/mensagem.php";
+	//print_r($caminho); 
+
+	$user = $data['user'];
+?>
+<h2>Editar usuário</h2>
+
+<p/>
+	<form action="./UserController.php?action=update&id=<?= $user->getId()?>" method="POST">
+		Nome: <input type="text" name="nome" value="<?= $user->getNome(); ?>">
+		
+		<br>
+		Telefone: <input type="text" name="telefone" value="<?= $user->getTelefone(); ?>">
+		
+		<br>
+		Email: <input type="text" name="email" value="<?= $user->getEmail(); ?>">
+		
+		<br>
+		Username: <input type="text" name="username" value="<?= $user->getUsername(); ?>">
+		<br>
+		
+		Data de nascimento: <input type="text" name="data de nascimento" value="<?= $user->getUsername(); ?>">
+		<br>
+		
+		Senha: <input type="text" name="senha" value="<?= $user->getSenha(); ?>">
+		
+		<p/>
+		<input type="submit" value="Atualizar">
+		<input type="reset" value="Limpar">
+	</form>		
+
+</body>
+</html>
