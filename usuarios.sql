08103207451ae987097f1def5c528ed799d7448d
Annalu-R <55161495+Annalu-R@users.noreply.github.com>
2022-09-03 21:56:31 -0300

    Update and rename cliente.sql to usuarios.sql

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@ mysql -u root -p // a senha eh bancodedados
 create database pvnn;
 use pvnn;
 
-CREATE TABLE clientes (
+CREATE TABLE usuarios (
 	id int(4) NOT NULL AUTO_INCREMENT,
 	nome varchar(50),
 	telefone varchar(14),
@@ -12,5 +12,5 @@ CREATE TABLE clientes (
 );
 
 show tables;
-desc clientes;
-select * from tbcliente;
+desc usuarios;
+select * from tbusuarios;
